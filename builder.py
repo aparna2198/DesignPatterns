e1dee64a55fffdb2e7674954163cf7f53e1c08bc
aparna2198 <aparna.code98@gmail.com>
2025-07-31 11:05:07 +0530

    builder pattern

---
+++ builder.py
@@ -0,0 +1,82 @@
+# stripe’s API allows merchants to create Payment Intents with many optional parameters (currency, amount, customer, metadata, payment method types, etc.).
+
+# You need to design a PaymentIntentBuilder class in Python that:
+
+# Implements the Builder pattern so that developers can fluently build complex payment intent objects.
+# Must support the following optional properties:
+# amount
+# currency
+# customer_id
+# description
+# payment_methods (list)
+# metadata (dict)
+# The build() method should return a dictionary representing the final payload ready to send to Stripe’s API.
+# The builder should allow method chaining.
+
+class PaymentIntentBuilder:
+    def __init__(self):
+        self.payment_methods = []
+        self.amount = None
+        self.currency = None
+        self.metadata = None
+        self.customer_id = None
+        self.metadata = None
+
+    def set_amount(self, amount):
+        self.amount = amount
+        return self
+    
+    def set_currency(self, currency):
+        self.currency = currency
+        return self
+    
+    def set_customer_id(self, customer_id):
+        self.customer_id = customer_id
+        return self
+    
+    def set_description(self, description):
+        self.description = description
+        return self
+
+    def add_payment_method(self, payment_method):
+        self.payment_methods.append(payment_method)
+        return self
+    
+    def set_metadata(self,metadata):
+        self.metadata = metadata
+        return self
+    
+    def build(self):
+        return {
+            "amount" : self.amount,
+            "currency": self.currency,
+            "metadata" :self.metadata,
+            "payment_methods": self.payment_methods,
+            "customer_id" : self.customer_id,
+            "description":self.description
+        }
+
+
+payment_intent = (
+    PaymentIntentBuilder()
+        .set_amount(5000)
+        .set_currency("usd")
+        .set_customer_id("cus_12345")
+        .set_description("Subscription Payment")
+        .add_payment_method("card")
+        .add_payment_method("apple_pay")
+        .set_metadata({"order_id": "order_9876"})
+        .build()
+)
+
+print(payment_intent)
+
+
+
+    
+    
+    
+
+
+
+
