0e50aa7e06cf548cb7ca0b2a613100573c1f9a1e
aparna2198 <aparna.code98@gmail.com>
2025-08-18 21:32:05 +0530

    adaptor+strategy pattern

---
+++ adaptor_strategy.py
@@ -0,0 +1,55 @@
+
+
+# The Adapter Pattern lets your code talk to external systems with different interfaces as if they were all the same — like plugging any charger into any wall with the right adapter.
+class PaymentProcessor:
+    def pay(self, amount):
+        raise NotImplementedError
+        
+class StripeAPI:
+    def make_payment(self, money):
+        print(f"Stripe paid {money}")
+
+class RazorPayAPI:
+    def do_transaction(self, money):
+        print(f"Razor pay paid {money}")
+
+class StripeAdaptor(PaymentProcessor):
+    def __init__(self, stripe:StripeAPI):
+        self.stripe = stripe
+    
+    def pay(self, amount):
+        self.stripe.make_payment(amount)
+
+class RazorAdaptor(PaymentProcessor):
+    def __init__(self, razorpay:RazorPayAPI):
+        self.razorpay = razorpay
+    
+    def pay(self, amount):
+        self.razorpay.do_transaction(amount)
+
+class Payment:
+    def __init__(self, strategy:PaymentProcessor):
+        self.strategy = strategy
+    
+    def set_strategy(self, strategy:PaymentProcessor):
+        self.strategy = strategy
+    
+    def checkout(self, amount):
+        self.strategy.pay(amount)
+
+
+stripe = StripeAdaptor(StripeAPI())
+razor = RazorAdaptor(RazorPayAPI())
+payment = Payment(stripe)
+payment.checkout(111)
+payment.set_strategy(razor)
+payment.checkout(2000)
+
+# stripe.pay(111)
+# razor.pay(123)
+
+
+
+
+        
+        
